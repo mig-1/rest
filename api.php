8d48cc10e70317cbfe19e0cd35f27b4dd8f6e824
Yura K <mig-1@yandex.ru>
2015-09-25 02:36:07 +0300

    Add work with CSV

--- api.php
+++ api.php
@@ -51,8 +51,9 @@ class MyAPI extends API
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
-
-var_dump($_REQUEST);
+if ( !strpos( $haystack, $needle ) ) {
+    var_dump($_REQUEST);
+}
 try {
     $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
     echo $API->processAPI();
